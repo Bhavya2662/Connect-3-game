732cabfaffba9b9dbdcdf8311b6877b739b4b893
Bhavya Sharma <bhavyaas2662@hotmail.com>
2021-02-07 22:05:32 +0530

    changed pngs

--- app/src/main/java/com/example/connectgame/MainActivity.java
+++ app/src/main/java/com/example/connectgame/MainActivity.java
@@ -23,44 +23,7 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};//2 means unplayed.
     int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
-//    public void dropIn(View view) {
-//        ImageView counter = (ImageView) view;
-//
-//        int tappedCounter = Integer.parseInt(counter.getTag().toString());
-//
-//        if (gameState[tappedCounter] == 2) {
-//            gameState[tappedCounter] = activePlayer;
-//
-//            counter.setTranslationY(-1000f);
-//            if (activePlayer == 0) {
-//                counter.setImageResource(R.drawable.blue);
-//                activePlayer = 1;
-//            } else {
-//
-//                counter.setImageResource(R.drawable.red);
-//                activePlayer = 0;
-//            }
-//            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
-//
-//
-//        }
-//        for (int[] winningPosition : winningPositions) {
-//            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-//                gameIsActive = false;
-//                String winner = "Red";
-//
-//                if (gameState[winningPosition[0]] == 0) {
-//                    winner = "Blue";
-//                }
-//
-//                TextView textView = findViewById(R.id.textView);
-//                textView.setText(winner + " has won!");
-//                LinearLayout linearLayout = findViewById(R.id.play_again);
-//                linearLayout.setVisibility(VISIBLE);
-//
-//            }
-//        }
-//    }
+
     public void dropIn(View view){
         ImageView img = (ImageView) view;
         int tappedImage = Integer.parseInt(img.getTag().toString());
@@ -154,29 +117,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
-
-//    public void playAgain(View view) {
-//        //make dialog invisible
-//        LinearLayout linearLayout = findViewById(R.id.play_again);
-//        linearLayout.animate().alpha(0).setDuration(100);
-//
-//        gameIsActive = true;
-//        activePlayer = 0;
-//        //setting gameState as unplayed
-//        for(int i = 0; i<gameState.length; i++){
-//            gameState[i] = 2;
-//        }
-//        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
-//        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
-//    }
 
 
     @Override
